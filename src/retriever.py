12acba16834e80586510c735b427630741ffdad7
techswarn <tech.swarns@gmail.com>
2025-01-17 20:17:01 +0530

    code update

--- src/retriever.py
+++ src/retriever.py
@@ -55,10 +55,12 @@ def build_vector_store_fromurl(content):
     
     if content:
         # If the vector store is not already present in the session state
-        if not st.session_state.vector_store:
+        
+ #       if not st.session_state.vector_store:
+        if 'vector_store' not in st.session_state:
             with st.spinner(text=":red[Please wait while we fetch the information...]"):
                 embeddings = HuggingFaceEmbeddings()
-                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30)
                 docs = text_splitter.split_documents(content)
                 vector_store = SKLearnVectorStore.from_documents(
                     documents=docs,
