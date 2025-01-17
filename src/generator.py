6eccf11cb66caa65e2788edd0cb749fa72f29749
techswarn <tech.swarns@gmail.com>
2025-01-17 16:42:26 +0530

    code update

--- src/generator.py
+++ src/generator.py
@@ -10,7 +10,7 @@ from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 def llm_instance():
-    model_id = "meta-llama/Meta-Llama-3-8B"
+    model_id = "meta-llama/Llama-3.1-8B-Instruct"
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     model = AutoModelForCausalLM.from_pretrained(model_id)
     pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=200)
